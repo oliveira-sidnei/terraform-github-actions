1cb7ac9fd2e35208c4a384dcbac66fe68ed0dca4
sohs3411 <oliveira.sidnei93@gmail.com>
2023-10-10 18:38:35 -0300

    Creating vpc resources
    * Implementing changes to terraform apply and plan.
    
    * Creating vpc resources
    
    * Adding local values to availability zones
    
    * Fixing indentation
    
    * Testing arn removal.
    
    * Removing resources from provider file.
    
    * Adding assume_role and external_id.
    
    * Fixing syntax
    
    * Changing role_arn inside provider file.
    
    * Removing assume_role configuration from provider.
    
    ---------
    
    Co-authored-by: Sidnei Santos <sidneiohs@gmail.com>

---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default = "sa-east-1"
+}
+
+locals {
+  availability_zones = ["${var.aws_region}a", "${var.aws_region}b"]
+}
+
+variable "environment" {
+  default = "terraform-aws-sohs"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.0.0/20", "10.0.128.0/20"]
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.16.0/20", "10.0.144.0/20"]
+  description = "CIDR block for Private Subnet"
+}
